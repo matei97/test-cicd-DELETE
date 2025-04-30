387757504ea7f195253873cd25575990d8c44593
Matei Madalin <matei.madalin1@gmail.com>
2025-04-30 18:30:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // Define a route
 app.get('/', (req, res) => {
-  res.send('Aplicația rulează. Felicitări!');
+  res.send('Aplicația rulează. Felicitări! V2');
 });
 
 // Start the server
